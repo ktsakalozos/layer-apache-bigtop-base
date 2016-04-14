78da2e1dabbca059332177f4ddb2892e69b4c367
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2016-04-14 12:31:03 -0600

    Tested refactored bigtop base

--- lib/charms/layer/apache_bigtop_base.py
+++ lib/charms/layer/apache_bigtop_base.py
@@ -7,7 +7,6 @@ from path import Path
 from charms import layer
 from charmhelpers.fetch.archiveurl import ArchiveUrlFetchHandler
 from jujubigdata import utils
-from charmhelpers.core import unitdata, hookenv
 from charmhelpers.core.host import chdir
 
 
@@ -15,7 +14,8 @@ class Bigtop(object):
 
     def __init__(self):
         self.bigtop_dir = '/home/ubuntu/bigtop.release'
-        self.bigtop_version = layer.option('bigtop_version')
+        self.options = layer.options('apache-bigtop-base')
+        self.bigtop_version = self.options.get('bigtop_version')
 
     def install(self, NN=None, RM=None):
         self.fetch_bigtop_release()
@@ -39,9 +39,9 @@ class Bigtop(object):
 
     def setup_puppet_config(self, NN, RM):
         # generate site.yaml. Something like this would do
-        hiera_dst = hookenv.config('bigtop_hiera_path')
-        hiera_conf = hookenv.config('bigtop_hiera_config')
-        hiera_site_yaml = hookenv.config('bigtop_hiera_siteyaml')
+        hiera_dst = self.options.get('bigtop_hiera_path')
+        hiera_conf = self.options.get('bigtop_hiera_config')
+        hiera_site_yaml = self.options.get('bigtop_hiera_siteyaml')
         bigtop_site_yaml = "{0}/{1}/{2}".format(self.bigtop_dir, self.bigtop_version, hiera_site_yaml)
         self.prepare_bigtop_config(bigtop_site_yaml, NN, RM)
         # Now copy hiera.yaml to /etc/puppet & point hiera to use the above location as hieradata directory
@@ -68,7 +68,7 @@ class Bigtop(object):
 
     def fetch_bigtop_release(self):
         # download Bigtop release; unpack the recipes
-        bigtop_url = layer.option('bigtop_release_url')
+        bigtop_url = self.options.get('bigtop_release_url')
         Path(self.bigtop_dir).rmtree_p()
         au = ArchiveUrlFetchHandler()
         au.install(bigtop_url, self.bigtop_dir)
@@ -77,17 +77,17 @@ class Bigtop(object):
         # TODO storage dirs should be configurable
         # TODO list of cluster components should be configurable
         localhost = subprocess.check_output(['hostname', '-f']).strip().decode()
-        if NN == None:
+        if NN is None:
             nn_hostname = localhost
         else:
             nn_hostname = NN
-        if RM == None:
+        if RM is None:
             rm_hostname = localhost
         else:
             rm_hostname = RM
-        java_package_name = hookenv.config('java_package_name')
+        java_package_name = self.options.get('java_package_name')
         # TODO figure out how to distinguish between different platforms
-        bigtop_apt = hookenv.config('bigtop_1.1.0_repo-x86_64')
+        bigtop_apt = self.options.get('bigtop_1.1.0_repo-x86_64')
 
         yaml_data = {
             'bigtop::hadoop_head_node': nn_hostname,
@@ -101,3 +101,7 @@ class Bigtop(object):
         Path(hr_conf).dirname().makedirs_p()
         with open(hr_conf, 'w+') as fd:
             yaml.dump(yaml_data, fd)
+
+
+def get_bigtop_base():
+    return Bigtop()
