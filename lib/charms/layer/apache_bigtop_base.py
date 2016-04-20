cfc2ac3e9ecfacbc99820164ad0d90efd1bcb64a
Kevin W Monroe <kevin.monroe@canonical.com>
2016-04-20 15:21:06 -0500

    condense role/component methods that share common code; component installs should now pass a host dict

--- lib/charms/layer/apache_bigtop_base.py
+++ lib/charms/layer/apache_bigtop_base.py
@@ -20,18 +20,11 @@ class Bigtop(object):
         self.bigtop_version = self.options.get('bigtop_version')
         self.bigtop_base = Path(self.bigtop_dir) / self.bigtop_version
 
-    # install role
-    def install(self, hosts, roles):
+    def install(self, hosts, roles=None):
+        '''Fetch, install, setup puppet config, and run puppet apply.'''
         self.fetch_bigtop_release()
         self.install_puppet_modules()
-        self.setup_puppet_role(hosts, roles)
-        self.trigger_puppet()
-
-    # install component
-    def install_component(self, NN=None, RM=None):
-        self.fetch_bigtop_release()
-        self.install_puppet_modules()
-        self.setup_puppet_component(NN, RM)
+        self.setup_puppet(hosts, roles)
         self.trigger_puppet()
 
     def trigger_puppet(self):
@@ -39,7 +32,7 @@ class Bigtop(object):
         charm_dir = hookenv.charm_dir()
         rm_patch = Path(charm_dir) / 'resources/patch1_rm_init_hdfs.patch'
         # nm_patch = Path(charm_dir) / 'resources/patch2_nm_core-site.patch'
-        with chdir("{0}/{1}".format(self.bigtop_dir, self.bigtop_version)):
+        with chdir("{}".format(self.bigtop_base)):
             # rm patch goes first
             utils.run_as('root', 'patch', '-p1', '-s', '-i', rm_patch)
             # utils.run_as('root', 'patch', '-p1', '-s', '-i', nm_patch)
@@ -64,28 +57,18 @@ class Bigtop(object):
         utils.run_as('hdfs', 'hdfs', 'dfs', '-mkdir', '-p', '/user/ubuntu')
         utils.run_as('hdfs', 'hdfs', 'dfs', '-chown', 'ubuntu', '/user/ubuntu')
 
-    def setup_puppet_role(self, hosts, roles):
-        # generate site.yaml. Something like this would do
+    def setup_puppet(self, hosts, roles=None):
+        # generate site.yaml for either role or component based installation
         hiera_dst = self.options.get('bigtop_hiera_path')
         hiera_conf = self.options.get('bigtop_hiera_config')
         hiera_site_yaml = self.options.get('bigtop_hiera_siteyaml')
-        bigtop_site_yaml = "{0}/{1}/{2}".format(self.bigtop_dir, self.bigtop_version, hiera_site_yaml)
-        self.prepare_bigtop_role(bigtop_site_yaml, hosts, roles)
+        bigtop_site_yaml = "{0}/{1}".format(self.bigtop_base, hiera_site_yaml)
+        if roles is None:
+            self.prepare_bigtop_component(bigtop_site_yaml, hosts)
+        else:
+            self.prepare_bigtop_role(bigtop_site_yaml, hosts, roles)
         # Now copy hiera.yaml to /etc/puppet & point hiera to use the above location as hieradata directory
-        Path("{0}/{1}/{2}".format(self.bigtop_dir, self.bigtop_version, hiera_conf)).copy(hiera_dst)
-        utils.re_edit_in_place(hiera_dst, {
-            r'.*:datadir.*': "  :datadir: {0}/".format(os.path.dirname(bigtop_site_yaml)),
-        })
-
-    def setup_puppet_component(self, NN, RM):
-        # generate site.yaml. Something like this would do
-        hiera_dst = self.options.get('bigtop_hiera_path')
-        hiera_conf = self.options.get('bigtop_hiera_config')
-        hiera_site_yaml = self.options.get('bigtop_hiera_siteyaml')
-        bigtop_site_yaml = "{0}/{1}/{2}".format(self.bigtop_dir, self.bigtop_version, hiera_site_yaml)
-        self.prepare_bigtop_component(bigtop_site_yaml, NN, RM)
-        # Now copy hiera.yaml to /etc/puppet & point hiera to use the above location as hieradata directory
-        Path("{0}/{1}/{2}".format(self.bigtop_dir, self.bigtop_version, hiera_conf)).copy(hiera_dst)
+        Path("{0}/{1}".format(self.bigtop_base, hiera_conf)).copy(hiera_dst)
         utils.re_edit_in_place(hiera_dst, {
             r'.*:datadir.*': "  :datadir: {0}/".format(os.path.dirname(bigtop_site_yaml)),
         })
@@ -144,37 +127,26 @@ class Bigtop(object):
         with open(hr_conf, 'w+') as fd:
             yaml.dump(yaml_data, fd)
 
-    def prepare_bigtop_component(self, hr_conf, NN=None, RM=None):
+    def prepare_bigtop_component(self, hr_conf, hosts={}):
         '''
-        NN: fqdn of the namenode (head node)
-        RM: fqdn of the resourcemanager (optional)
-        extra: list of extra cluster components
+        :param hosts: dict of {service: fqdn}
         '''
-        # TODO storage dirs should be configurable
         cluster_components = self.options.get("bigtop_component_list").split()
-        # Setting NN (our head node) is required; exit and log if we dont have it
-        if NN is None:
-            nn_fqdn = ''
-            hookenv.log("No NN hostname given for install")
-        else:
-            nn_fqdn = NN
-            hookenv.log("Using %s as our hadoop_head_node" % nn_fqdn)
-
-        # If we have an RM, add 'yarn' to the installed components
-        if RM is None:
-            rm_fqdn = ''
-            hookenv.log("No RM hostname given for install")
-        else:
-            rm_fqdn = RM
-
         java_package_name = self.options.get('java_package_name')
         bigtop_apt = self.options.get('bigtop_repo-{}'.format(utils.cpu_arch()))
-
         gw_host = subprocess.check_output(['facter', 'fqdn']).strip().decode()
+        nn_host = ''
+        rm_host = ''
+        for k, host in hosts.items():
+            if k == 'namenode':
+                nn_host = host
+            elif k == 'resourcemanager':
+                rm_host = host
+
         yaml_data = {
             'bigtop::hadoop_gateway_node': gw_host,
-            'bigtop::hadoop_head_node': nn_fqdn,
-            'hadoop::common_yarn::hadoop_rm_host': rm_fqdn,
+            'bigtop::hadoop_head_node': nn_host,
+            'hadoop::common_yarn::hadoop_rm_host': rm_host,
             'hadoop::hadoop_storage_dirs': ['/data/1', '/data/2'],
             'hadoop_cluster_node::cluster_components': cluster_components,
             'bigtop::jdk_package_name': '{0}'.format(java_package_name),
